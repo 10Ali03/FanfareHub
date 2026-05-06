beb14a647f74f73ceea9b96bc3cd26adfc51a282
Nawal <nana.miwa@outlook.fr>
2026-05-06 10:17:27 +0200

    organisation code

--- WEB-INF/classes/dao/Fanfaron.java
+++ WEB-INF/classes/dao/Fanfaron.java
@@ -1,4 +1,4 @@
-package metier;
+package dao;
 import java.sql.Timestamp;
 
 public class Fanfaron {
--- WEB-INF/classes/dao/FanfaronDAO.java
+++ WEB-INF/classes/dao/FanfaronDAO.java
@@ -4,9 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
-
-import metier.Fanfaron;
 
 public class FanfaronDAO {
     private final DbConnectionManager dbConnectionManager;
